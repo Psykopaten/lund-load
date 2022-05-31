721cf8eaf551ca547727e5e5af38bee7865c79a2
Psykopaten <104300989+Psykopaten@users.noreply.github.com>
2022-05-31 21:14:55 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+version '1.0.0'
+author 'Psykopaten'
+description 'LoadingScreen'
+
+files {
+    'index.html',
+    'main.css',
+    'background.png',
+    'logo.png',
+    'load.mp3',
+    "js/music-controls.js",
+    "js/music-handler.js"
+}
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+game 'gta5'
